5d40c7d2dba14faeca389e6e9054277ac677a236
CodigoMorrazo <ipedrosacip@gmail.com>
2020-12-20 20:40:39 +0100

    Added bus structure

--- chatbot/infrastructure/telegramRepository.py
+++ chatbot/infrastructure/telegramRepository.py
@@ -1,1 +1,13 @@
 #python3 wrapper/conector with telegram
+
+class TelegramRepository:
+	def __main__(self):
+		pass
+		# Todo implement as a service listening for entries
+
+	def greetings(self, userAlias):
+		command = {
+			"id": "", #generate uuid
+			"commandName": "command.greeting.generic",
+			"userAlias": userAlias
+		}
